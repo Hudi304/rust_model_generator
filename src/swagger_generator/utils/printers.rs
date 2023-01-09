ab356f2b68f1740c9715370732f32a51c1adb9f6
Hudi304 <hudisteanum@yahoo.com>
2023-01-09 12:01:17 +0200

    Refactor read toml

--- src/swagger_generator/utils/printers.rs
+++ src/swagger_generator/utils/printers.rs
@@ -1,8 +1,6 @@
-use std::io::Error;
-
 use colored::Colorize;
 
-pub fn print_delete_file_error(error: Error, file_path: &String) {
+pub fn print_delete_file_error(error: std::io::Error, file_path: &String) {
     println!(
         "{} {} {}",
         "ERROR on file DELETE".red(),
@@ -15,14 +13,26 @@ pub fn print_delete_file_success(file_path: &String) {
     println!("{} {}", "Deleted File :".yellow(), &file_path);
 }
 
-pub fn print_create_file_error(error: Error, file_path: &String) {
+pub fn print_create_file_error(error: std::io::Error, file_path: &String) {
     let message = "Error During File Creation :".yellow();
     println!("{message} {file_path} \n : {}", error.to_string());
 }
 
-pub fn print_write_file_error(error: Error, file_path: &String) {
+pub fn print_write_file_error(error: std::io::Error, file_path: &String) {
     let message = "Error During File Creation :".yellow();
     println!("{message} {file_path} \n : {}", error.to_string());
+}
+
+pub fn print_read_file_error(file_path: &String, error: std::io::Error) {
+    let message = "Could not Read File".red();
+    let file_path = file_path.white();
+    println!("{message} {file_path} \n : {error}");
+}
+
+pub fn print_parsing_error(file_path: &String, error: toml::de::Error) {
+    let message = "Could parse file contents of".red();
+    let file_path = file_path.white();
+    println!("{message} {file_path} \n : {error}");
 }
 
 pub fn print_write_file_success(file_path: &String) {
--- src/swagger_generator/utils/read_toml.rs
+++ src/swagger_generator/utils/read_toml.rs
@@ -1,6 +1,5 @@
-use std::fs;
-
-use colored::Colorize;
+use super::printers::{print_parsing_error, print_read_file_error};
+use std::{fs, process};
 
 #[derive(serde::Deserialize)]
 pub struct ReplaceAllSettings {
@@ -12,40 +11,33 @@ pub struct ReplaceAllSettings {
 
 pub struct GeneratorConfig {}
 
-pub fn read_generator_config() -> GeneratorConfig {
-    let settings_path = "src/tools/replace_all_config.toml";
+pub fn read_generator_config(generator_path: &String) -> GeneratorConfig {
+    let file = fs::read_to_string(generator_path);
 
-    let file: String = match fs::read_to_string(settings_path) {
-        Err(why) => {
-            panic!("{}: {}", "couldn't open \n".red(), why);
-        }
-        Ok(file) => file,
-    };
+    let file = file.unwrap_or_else(|err| {
+        print_read_file_error(generator_path, err);
+        process::exit(1)
+    });
 
-    let file_clone = file.clone();
+    let config = toml::from_str::<GeneratorConfig>(&file);
 
-    let config: GeneratorConfig = match toml::from_str(&file_clone) {
-        Ok(d) => d,
-        Err(_) => {
-            panic!("Unable to load data from ");
-        }
-    };
+    let config = config.unwrap_or_else(|err| {
+        print_parsing_error(generator_path, err);
+        process::exit(1)
+    });
 
     return config;
 }
 
 // pub fn read_toml<'a, T: serde::Deserialize<'a>>() -> impl serde::Deserialize<'a> {
 //     let settings_path = "src/tools/replace_all_config.toml";
-
 //     let file: String = match fs::read_to_string(settings_path) {
 //         Err(why) => {
 //             panic!("{}: {}", "couldn't open \n".red(), why);
 //         }
 //         Ok(file) => file,
 //     };
-
 //     let file_clone = file.clone();
-
 //     // how do you fix this?
 //     let config: T = match toml::from_str(&file_clone) {
 //         Ok(d) => d,
@@ -53,6 +45,5 @@ pub fn read_generator_config() -> GeneratorConfig {
 //             panic!("Unable to load data from ");
 //         }
 //     };
-
 //     return config;
 // }
